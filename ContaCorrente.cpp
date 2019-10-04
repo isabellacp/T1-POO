48c0c7a151f64cec88a4dae059d989ae278ff315
Isabella Costa <isabellacostap@gmail.com>
2019-10-04 19:02:40 -0300

    criar funções debitoConta e creditoConta, criar struct para o extrato

--- ContaCorrente.cpp
+++ ContaCorrente.cpp
@@ -2,54 +2,77 @@
 // Created by isabe on 03/10/2019.
 //
 
+#include <string>
 #include "ContaCorrente.h"
 int ContaCorrente::NumeroGlobal =0;
-ContaCorrente::ContaCorrente(string cpf_cliente)  {
+ContaCorrente::ContaCorrente(string cpf_cliente) {
     Numero = NumeroGlobal++; // contador de conta corrente: cada vez que instanciar a classe, gera um novo numero de conta (evita contas com numeração repetida)
     CPFcliente = cpf_cliente;
     SaldoAtual = 0;
     DataAbertura = time(0); //retorna a data atual
 }
 
+
+
 char *ContaCorrente::GetDataAbertura() {
     return (char *) ctime(&DataAbertura);
 }
 
-void ContaCorrente::FazerLancamento() {
 
+
+void ContaCorrente::FazerLancamento(int tipo, float valor) {
+    switch (tipo) {
+        case 1: {
+            SaldoAtual = debitoConta(valor);
+            case 2: {
+                SaldoAtual = creditoConta(valor);
+            }
+        }
+    }
 }
+    void ContaCorrente::ImprimirExtrato() {
 
-void ContaCorrente::ImprimirExtrato(){
+    }
 
-}
-
-int ContaCorrente::GetNumero() {
-    return Numero;
-}
+    int ContaCorrente::GetNumero() {
+        return Numero;
+    }
 
 
 //getters e setters
-const string &ContaCorrente::getCpFcliente() const {
-    return CPFcliente;
-}
+    const string &ContaCorrente::getCpFcliente() const {
+        return CPFcliente;
+    }
+    void ContaCorrente::setCpFcliente(const string &cpFcliente) {
+        CPFcliente = cpFcliente;
+    }
 
-void ContaCorrente::setCpFcliente(const string &cpFcliente) {
-    CPFcliente = cpFcliente;
-}
+    time_t ContaCorrente::getDataAbertura() const {
+        return DataAbertura;
+    }
+    void ContaCorrente::setDataAbertura(time_t dataAbertura) {
+        DataAbertura = dataAbertura;
+    }
 
-time_t ContaCorrente::getDataAbertura() const {
-    return DataAbertura;
-}
+    float ContaCorrente::getSaldoAtual() const {
+        return SaldoAtual;
+    }
+    void ContaCorrente::setSaldoAtual(float saldoAtual) {
+        SaldoAtual = saldoAtual;
+    }
+//debita o valor da conta
+    float ContaCorrente::debitoConta(float valor) {
+        return SaldoAtual -= valor;
+    }
+//credita um valor na conta
+    float ContaCorrente::creditoConta(float valor) {
+        return SaldoAtual += valor;
+    }
 
-void ContaCorrente::setDataAbertura(time_t dataAbertura) {
-    DataAbertura = dataAbertura;
-}
+    void ContaCorrente::AlterarDados(string cpf_cliente) {
 
-float ContaCorrente::getSaldoAtual() const {
-    return SaldoAtual;
-}
+    }
 
-void ContaCorrente::setSaldoAtual(float saldoAtual) {
-    SaldoAtual = saldoAtual;
-}
-
+    string ContaCorrente::toString() {
+        return string();
+    }
--- ContaCorrente.h
+++ ContaCorrente.h
@@ -10,35 +10,44 @@
 
 using  namespace std;
 
-struct LANCAMENTO{
-    string tipo;
-    int valor;
-    time_t DataLancamento;
+struct List{
+    string type;
+    int valor = 0;
+    time_t DataLancamento = time (0);
+    int size=0; ///tamanho do array
+    string array[sizeof(size)];
 };
 
 class ContaCorrente {
 public:
     //construtor
     ContaCorrente(string cpf_cliente);
+
+    struct List  ext;
+
     //getter - retorna uma representação em string da data de abertura da conta
     char *GetDataAbertura();
     int GetNumero();
-    void FazerLancamento();
+    void FazerLancamento(int, float);
     void ImprimirExtrato();
     void AlterarDados(string cpf_cliente);
+    float debitoConta (float);
+    float creditoConta (float);
     string toString();
 
+    //getter e setter para o CPF do cliente
     const string &getCpFcliente() const;
-
     void setCpFcliente(const string &cpFcliente);
 
+    //getter e setter para a Data de Abertura da conta
     time_t getDataAbertura() const;
-
     void setDataAbertura(time_t dataAbertura);
 
+    //getter e setter para o SaldoAtual
     float getSaldoAtual() const;
-
     void setSaldoAtual(float saldoAtual);
+
+
 
 private:
     static int NumeroGlobal;
@@ -48,5 +57,7 @@ private:
     float SaldoAtual;
 
 };
+
+
 
 #endif //T1_POO_CONTACORRENTE_H
